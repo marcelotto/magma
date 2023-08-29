16e233dd50b3cd4c411d8d14c1786216d68d9c4f
Marcel Otto <marcelotto@gmx.de>
2023-08-29 12:59:44 +0200

    Add button to execute prompts

---
+++ lib/mix/tasks/magma/prompt/exec.ex
@@ -0,0 +1,28 @@
+defmodule Mix.Tasks.Magma.Prompt.Exec do
+  @shortdoc "Executes a prompt"
+  @moduledoc @shortdoc
+
+  use Mix.Task
+
+  import Magma.MixHelper
+
+  alias Magma.Artefact
+
+  @options []
+
+  def run(args) do
+    Mix.Task.run("app.start")
+
+    with_valid_options(args, @options, fn
+      _opts, [] ->
+        Mix.shell().error("prompt name or path missing")
+
+      _opts, [prompt_name] ->
+        {:ok, _} =
+          prompt_name
+          |> Artefact.Prompt.load!()
+          |> Artefact.PromptResult.new!()
+          |> Artefact.PromptResult.create()
+    end)
+  end
+end
--- priv/templates/artefact_prompt/module/ModuleDoc.md
+++ priv/templates/artefact_prompt/module/ModuleDoc.md
@@ -6,6 +6,7 @@ created_at: <%= prompt.created_at %>
 tags: <%= yaml_list(prompt.tags) %>
 aliases: <%= yaml_list(prompt.aliases) %>
 ---
+<%= button("Execute", "magma.prompt.exec", color: "blue") %>
 <%= button("Update", "magma.prompt.update") %>
 
 # <%= prompt.name %>
