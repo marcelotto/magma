b7a807901b907ac905aeb359a106e9929e3b6da6
Marcel Otto <marcelotto@gmx.de>
2023-09-08 17:10:02 +0200

    Missing commit
    Changes on this file were missing in previous commits

--- lib/magma/artefacts/module/module_doc.ex
+++ lib/magma/artefacts/module/module_doc.ex
@@ -4,13 +4,26 @@ defmodule Magma.Artefacts.ModuleDoc do
   @type t :: %__MODULE__{}
 
   @impl true
-  def name(concept), do: "ModuleDoc of #{concept.name}"
+  def build_name(concept), do: "ModuleDoc of #{concept.name}"
 
-  def prompt_name(concept), do: "Prompt for #{name(concept)}"
+  def prompt_name(concept), do: "Prompt for #{build_name(concept)}"
+
+  def base_path(%__MODULE__{concept: concept}) do
+    Path.join(["modules", concept.name, "moduledoc"])
+  end
 
   @impl true
-  def prompt_path(%__MODULE__{concept: concept}) do
-    Path.join(["modules", concept.name, "moduledoc", "#{prompt_name(concept)}.md"])
+  def prompt_path(%__MODULE__{concept: concept} = moduledoc) do
+    moduledoc
+    |> base_path()
+    |> Path.join("#{prompt_name(concept)}.md")
+  end
+
+  @impl true
+  def version_path(%__MODULE__{concept: concept} = moduledoc) do
+    moduledoc
+    |> base_path()
+    |> Path.join("#{build_name(concept)}.md")
   end
 
   @impl true
