8d2b009d253dd239bfadad724358a46b23b314ab
Marcel Otto <marcelotto@gmx.de>
2023-10-04 16:06:40 +0200

    Use GPT-4 for Magma docs

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,9 @@
 import Config
 
+config :magma, Magma.Generation.OpenAI,
+  model: "gpt-4",
+  temperature: 0.2
+
 config :openai,
   api_key: {:system, "OPENAI_API_KEY"},
   organization_key: {:system, "OPENAI_ORGANIZATION_KEY"},
