d79e55db33b04faaf0d517fdff0d74981285ffc1
Marcel Otto <marcelotto@gmx.de>
2023-10-19 15:39:07 +0200

    Improve moduledoc prompt

--- lib/magma/artefacts/module/module_doc.ex
+++ lib/magma/artefacts/module/module_doc.ex
@@ -15,29 +15,40 @@ defmodule Magma.Artefacts.ModuleDoc do
   @impl true
   def system_prompt_task(_concept) do
     """
-    Your task is to write documentation for Elixir modules. The produced documentation is in English, clear, concise, comprehensible and follows the format in the following Markdown block (Markdown block not included):
+    You have two tasks to do based on the given implementation of the module and your knowledge base:
+
+    1. generate the content of the `@doc` strings of the public functions
+    2. generate the content of the `@moduledoc` string of the module to be documented
+
+    Each documentation string should start with a short introductory sentence summarizing the main function of the module or function. Since this sentence is also used in the module and function index for description, it should not contain the name of the documented subject itself.
+
+    After this summary sentence, the following sections and paragraphs should cover:
+
+    - What's the purpose of this module/function?
+    - For moduledocs: What are the main function(s) of this module?
+    - If possible, an example usage in an "Example" section using an indented code block
+    - configuration options (if there are any)
+    - everything else users of this module/function need to know (but don't repeat anything that's already obvious from the typespecs)
+
+    The produced documentation follows the format in the following Markdown block (Produce just the content, not wrapped in a Markdown block). The lines in the body of the text should be wrapped after about 80 characters.
 
     ```markdown
-    ## #{@prompt_result_section_title}
-
-    The first line should be a very short one-sentence summary of the main purpose of the module. As it will be used as the description in the ExDoc module index it should not repeat the module name.
-
-    Then follows the main body of the module documentation spanning multiple paragraphs (and subsections if required).
-
-
     ## Function docs
-
-    In this section the public functions of the module are documented in individual subsections. If a function is already documented perfectly, just write "Perfect!" in the respective section.
 
     ### `function/1`
 
-    The first line should be a very short one-sentence summary of the main purpose of this function.
+    Summary sentence
 
-    Then follows the main body of the function documentation.
+    Body
+
+    ## #{@prompt_result_section_title}
+
+    Summary sentence
+
+    Body
     ```
 
     #{View.comment("You can edit this prompt, as long you ensure the moduledoc is generated in a section named '#{@prompt_result_section_title}', as the contents of this section is used for the @moduledoc.")}
-
     """
     |> String.trim_trailing()
   end
