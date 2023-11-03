054c2524ff6596d848928bf23164cf7e6f8afcac
Marcel Otto <marcelotto@gmx.de>
2023-11-03 13:29:16 +0100

    Add ExDoc config

--- mix.exs
+++ mix.exs
@@ -1,6 +1,8 @@
 defmodule Magma.MixProject do
   use Mix.Project
 
+  @scm_url "https://github.com/marcelotto/magma"
+
   @version File.read!("VERSION") |> String.trim()
 
   def project do
@@ -18,7 +20,11 @@ defmodule Magma.MixProject do
         "vcr.delete": :test,
         "vcr.check": :test,
         "vcr.show": :test
-      ]
+      ],
+
+      # Docs
+      name: "Magma",
+      docs: docs()
     ]
   end
 
@@ -40,6 +46,119 @@ defmodule Magma.MixProject do
       {:ex_doc, "~> 0.30", only: :dev, runtime: false}
     ]
   end
+
+  defp docs do
+    [
+      main: "Magma",
+      source_url: @scm_url,
+      source_ref: "v#{@version}",
+      logo: "docs.magma/attachments/logo.png",
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"],
+      before_closing_head_tag: &before_closing_head_tag/1,
+      extra_section: "GUIDES",
+      extras: extras(),
+      groups_for_extras: groups_for_extras(),
+      groups_for_modules: [
+        Vault: [
+          Magma.Vault,
+          Magma.Vault.BaseVault
+        ],
+        Documents: [
+          Magma.Document,
+          Magma.Prompt,
+          Magma.Prompt.Template,
+          Magma.PromptResult,
+          Magma.Concept,
+          Magma.Concept.Template,
+          Magma.Artefact.Prompt,
+          Magma.Artefact.Version
+        ],
+        DocumentStruct: [
+          Magma.DocumentStruct,
+          Magma.DocumentStruct.Section
+        ],
+        Matter: [
+          Magma.Matter,
+          Magma.Matter.Module,
+          Magma.Matter.Project
+        ],
+        Artefacts: [
+          Magma.Artefact,
+          Magma.Artefacts.ModuleDoc,
+          Magma.Artefacts.Readme,
+          Magma.Artefacts.Article
+        ],
+        Text: [
+          Magma.Text,
+          Magma.Text.Preview,
+          Magma.Matter.Text,
+          Magma.Matter.Text.Section,
+          Magma.Matter.Text.Type,
+          Magma.Matter.Texts.Generic,
+          Magma.Matter.Texts.UserGuide,
+          Magma.Artefacts.TableOfContents
+        ],
+        Generation: [
+          Magma.Generation,
+          Magma.Generation.OpenAI,
+          Magma.Generation.Manual
+        ]
+      ]
+    ]
+  end
+
+  def extras() do
+    [
+      user_guide_page("Introduction to Magma"),
+      user_guide_page("Installation and setup"),
+      user_guide_page("Transclusion Resolution"),
+      user_guide_page("Custom Prompts and Prompt Execution"),
+      user_guide_page("Creating and Understanding Magma Artefacts"),
+      user_guide_page("Generating Complex Artefacts"),
+      user_guide_page("Current Limitations and Roadmap"),
+      "LICENSE.txt",
+      "CHANGELOG.md"
+    ]
+  end
+
+  defp user_guide_page(name) do
+    "docs.magma/artefacts/final/texts/Magma User Guide/article/Magma User Guide - #{name} (article section).md"
+  end
+
+  defp groups_for_extras do
+    [
+      "User Guide": ~r[docs.magma/artefacts/final/texts/Magma User Guide/article/.?]
+    ]
+  end
+
+  defp before_closing_head_tag(:html) do
+    """
+    <script src="https://cdn.jsdelivr.net/npm/mermaid@10.2.3/dist/mermaid.min.js"></script>
+    <script>
+      document.addEventListener("DOMContentLoaded", function () {
+        mermaid.initialize({
+          startOnLoad: false,
+          theme: document.body.className.includes("dark") ? "dark" : "default"
+        });
+        let id = 0;
+        for (const codeEl of document.querySelectorAll("pre code.mermaid")) {
+          const preEl = codeEl.parentElement;
+          const graphDefinition = codeEl.textContent;
+          const graphEl = document.createElement("div");
+          const graphId = "mermaid-graph-" + id++;
+          mermaid.render(graphId, graphDefinition).then(({svg, bindFunctions}) => {
+            graphEl.innerHTML = svg;
+            bindFunctions?.(graphEl);
+            preEl.insertAdjacentElement("afterend", graphEl);
+            preEl.remove();
+          });
+        }
+      });
+    </script>
+    """
+  end
+
+  defp before_closing_head_tag(:epub), do: ""
 
   defp elixirc_paths(:test), do: ["lib", "test/support", "test/modules"]
   defp elixirc_paths(_), do: ["lib"]
