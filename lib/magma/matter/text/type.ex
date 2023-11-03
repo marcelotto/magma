da6f71cff1401ccf50e708843981d0288c8287e4
Marcel Otto <marcelotto@gmx.de>
2023-11-03 02:52:29 +0100

    Fix Magma.Matter.Text.Type callback

--- lib/magma/matter/text/type.ex
+++ lib/magma/matter/text/type.ex
@@ -1,7 +1,7 @@
 defmodule Magma.Matter.Text.Type do
   @callback label :: binary
 
-  @callback system_prompt_task(Concept.t()) :: binary
+  @callback system_prompt_task(Magma.Concept.t()) :: binary
 
   defmacro __using__(_opts) do
     quote do
